99eff46f4b50d25b88a3783f7e9be1d1943e34b0
michaelwagih199 <michaelwagih199@gmail.com>
2021-04-01 10:35:54 +0200

    Initial commit

---
+++ app/src/main/java/com/polimigo/benevolent/models/Person.java
@@ -0,0 +1,66 @@
+package com.polimigo.benevolent.models;
+
+
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.Ignore;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "person")
+public class Person {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    @ColumnInfo(name = "name")
+    private String name;
+    @ColumnInfo(name = "number")
+    private String number;
+    @ColumnInfo(name = "notes")
+    private String notes;
+
+    public Person(int id, String name, String number, String notes) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.notes = notes;
+    }
+
+    @Ignore
+    public Person(String name, String number, String notes) {
+        this.name = name;
+        this.number = number;
+        this.notes = notes;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
